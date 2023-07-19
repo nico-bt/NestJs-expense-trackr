deef0cf97dc73c4e40cc637abe42b29b66bea374
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-07-19 17:52:49 -0300

    Add basic logic to all routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,11 +3,14 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Post,
   Put,
 } from '@nestjs/common';
 import { AppService } from './app.service';
+import { data, ReportType } from './data';
+import { randomUUID } from 'crypto';
 
 @Controller('report/:type')
 export class AppController {
@@ -19,27 +22,80 @@ export class AppController {
   // }
 
   @Get()
-  getAllReports(@Param('type') type) {
-    return `Reports of type: ${type}`;
+  getAllReports(@Param('type') type: string) {
+    if (type === ReportType.INCOME) {
+      return data.report.filter((item) => item.type === ReportType.INCOME);
+    }
+    if (type === ReportType.EXPENSE) {
+      return data.report.filter((item) => item.type === ReportType.EXPENSE);
+    }
+
+    return `Wrong type`;
   }
 
   @Get(':id')
-  getReportById(@Param('id') id) {
-    return `report with id: ${id}`;
+  getReportById(@Param('type') type: string, @Param('id') id: string) {
+    return data.report
+      .filter((item) => item.type === type)
+      .find((item) => item.id === id);
   }
 
   @Post()
   createReport(@Body() body, @Param('type') type) {
-    return `CREATE Report with type: ${type} and amount: ${body.amount}`;
+    const { source, amount } = body;
+
+    if (type === ReportType.INCOME || ReportType.EXPENSE) {
+      const newReport = {
+        id: randomUUID(),
+        source,
+        amount,
+        created_at: new Date(),
+        updated_at: new Date(),
+        type,
+      };
+
+      data.report.push(newReport);
+
+      return newReport;
+    } else {
+      return 'Wrong type';
+    }
   }
 
   @Put(':id')
   editReport(@Body() body, @Param('id') id, @Param('type') type) {
-    return `Edit Income in report with id: ${id}, amount: ${body.amount} and type: ${type}`;
+    const reportToUpdate = data.report
+      .filter((item) => item.type === type)
+      .find((item) => item.id === id);
+
+    if (!reportToUpdate) {
+      return 'Report Not found';
+    }
+
+    const reportToUpdateIndex = data.report.findIndex((item) => item.id === id);
+
+    const reportUpdated = {
+      ...reportToUpdate,
+      ...body,
+      updated_at: new Date(),
+    };
+
+    data.report[reportToUpdateIndex] = reportUpdated;
+
+    return reportUpdated;
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  deleteReport(@Param('id') id, @Param('type') type) {
-    return `DELETE REPORT WITH ID: ${id}`;
+  deleteReport(@Param('id') id: string) {
+    const reportIndex = data.report.findIndex((item) => item.id === id);
+
+    if (reportIndex === -1) {
+      return 'Not Found';
+    }
+
+    data.report.splice(reportIndex, 1);
+
+    return `DELETED`;
   }
 }
