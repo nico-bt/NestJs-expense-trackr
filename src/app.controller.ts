2a0f1b7a34cd6cd463d5a70dbf99aaa03761f1ca
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-07-19 22:57:11 -0300

    Move logic from controller to service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,93 +9,35 @@ import {
   Put,
 } from '@nestjs/common';
 import { AppService } from './app.service';
-import { data, ReportType } from './data';
-import { randomUUID } from 'crypto';
+import { ReportType } from './data';
 
 @Controller('report/:type')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
-
   @Get()
-  getAllReports(@Param('type') type: string) {
-    if (type === ReportType.INCOME) {
-      return data.report.filter((item) => item.type === ReportType.INCOME);
-    }
-    if (type === ReportType.EXPENSE) {
-      return data.report.filter((item) => item.type === ReportType.EXPENSE);
-    }
-
-    return `Wrong type`;
+  getAllReports(@Param('type') type: ReportType) {
+    return this.appService.getAllReports(type);
   }
 
   @Get(':id')
-  getReportById(@Param('type') type: string, @Param('id') id: string) {
-    return data.report
-      .filter((item) => item.type === type)
-      .find((item) => item.id === id);
+  getReportById(@Param('type') type: ReportType, @Param('id') id: string) {
+    return this.appService.getReportById(type, id);
   }
 
   @Post()
   createReport(@Body() body, @Param('type') type) {
-    const { source, amount } = body;
-
-    if (type === ReportType.INCOME || ReportType.EXPENSE) {
-      const newReport = {
-        id: randomUUID(),
-        source,
-        amount,
-        created_at: new Date(),
-        updated_at: new Date(),
-        type,
-      };
-
-      data.report.push(newReport);
-
-      return newReport;
-    } else {
-      return 'Wrong type';
-    }
+    return this.appService.createReport(body, type);
   }
 
   @Put(':id')
   editReport(@Body() body, @Param('id') id, @Param('type') type) {
-    const reportToUpdate = data.report
-      .filter((item) => item.type === type)
-      .find((item) => item.id === id);
-
-    if (!reportToUpdate) {
-      return 'Report Not found';
-    }
-
-    const reportToUpdateIndex = data.report.findIndex((item) => item.id === id);
-
-    const reportUpdated = {
-      ...reportToUpdate,
-      ...body,
-      updated_at: new Date(),
-    };
-
-    data.report[reportToUpdateIndex] = reportUpdated;
-
-    return reportUpdated;
+    return this.appService.editReport(body, id, type);
   }
 
   @HttpCode(204)
   @Delete(':id')
   deleteReport(@Param('id') id: string) {
-    const reportIndex = data.report.findIndex((item) => item.id === id);
-
-    if (reportIndex === -1) {
-      return 'Not Found';
-    }
-
-    data.report.splice(reportIndex, 1);
-
-    return `DELETED`;
+    return this.appService.deleteReport(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,83 @@
 import { Injectable } from '@nestjs/common';
+import { ReportType, data } from './data';
+import { randomUUID } from 'crypto';
+
+interface ReportData {
+  amount: number;
+  source: string;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return '<h1>Hello World! / Hola Mundo! / Allo monde!</h1>';
+  getAllReports(type: ReportType) {
+    if (type === ReportType.INCOME) {
+      return data.report.filter((item) => item.type === ReportType.INCOME);
+    }
+    if (type === ReportType.EXPENSE) {
+      return data.report.filter((item) => item.type === ReportType.EXPENSE);
+    }
+
+    return `Wrong type`;
+  }
+
+  getReportById(type: ReportType, id: string) {
+    return data.report
+      .filter((item) => item.type === type)
+      .find((item) => item.id === id);
+  }
+
+  createReport(body: ReportData, type: ReportType) {
+    const { source, amount } = body;
+
+    if (type === ReportType.INCOME || ReportType.EXPENSE) {
+      const newReport = {
+        id: randomUUID(),
+        source,
+        amount,
+        created_at: new Date(),
+        updated_at: new Date(),
+        type,
+      };
+
+      data.report.push(newReport);
+
+      return newReport;
+    } else {
+      return 'Wrong type';
+    }
+  }
+
+  editReport(body: ReportData, id: string, type: ReportType) {
+    const reportToUpdate = data.report
+      .filter((item) => item.type === type)
+      .find((item) => item.id === id);
+
+    if (!reportToUpdate) {
+      return 'Report Not found';
+    }
+
+    const reportToUpdateIndex = data.report.findIndex((item) => item.id === id);
+
+    const reportUpdated = {
+      ...reportToUpdate,
+      ...body,
+      updated_at: new Date(),
+    };
+
+    data.report[reportToUpdateIndex] = reportUpdated;
+
+    return reportUpdated;
+  }
+
+  deleteReport(id: string) {
+    const reportIndex = data.report.findIndex((item) => item.id === id);
+
+    if (reportIndex === -1) {
+      return 'Not Found';
+    }
+
+    data.report.splice(reportIndex, 1);
+
+    return `DELETED`;
   }
 }
