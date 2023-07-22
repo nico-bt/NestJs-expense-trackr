16b2b45f7d13ef7277579db77bf8c5b1fa09e294
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-07-22 00:02:04 -0300

    Add response dto as returning object

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ReportType, data } from './data';
 import { randomUUID } from 'crypto';
+import { ReportResponseDto } from './dtos/report.dto';
 
 interface ReportData {
   amount: number;
@@ -14,51 +15,51 @@ interface UpdateReportData {
 
 @Injectable()
 export class AppService {
-  getAllReports(type: ReportType) {
-    if (type === ReportType.INCOME) {
-      return data.report.filter((item) => item.type === ReportType.INCOME);
-    }
-    if (type === ReportType.EXPENSE) {
-      return data.report.filter((item) => item.type === ReportType.EXPENSE);
-    }
+  getAllReports(type: ReportType): ReportResponseDto[] {
+    const reportsArray = data.report.filter((item) => item.type === type);
 
-    return `Wrong type`;
+    return reportsArray.map((item) => new ReportResponseDto(item));
   }
 
-  getReportById(type: ReportType, id: string) {
-    return data.report
+  getReportById(type: ReportType, id: string): ReportResponseDto {
+    const report = data.report
       .filter((item) => item.type === type)
       .find((item) => item.id === id);
+
+    if (!report) return;
+
+    return new ReportResponseDto(report);
   }
 
-  createReport(body: ReportData, type: ReportType) {
+  createReport(body: ReportData, type: ReportType): ReportResponseDto {
     const { source, amount } = body;
 
-    if (type === ReportType.INCOME || ReportType.EXPENSE) {
-      const newReport = {
-        id: randomUUID(),
-        source,
-        amount,
-        created_at: new Date(),
-        updated_at: new Date(),
-        type,
-      };
+    const newReport = {
+      id: randomUUID(),
+      source,
+      amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type,
+    };
 
-      data.report.push(newReport);
+    data.report.push(newReport);
 
-      return newReport;
-    } else {
-      return 'Wrong type';
-    }
+    return new ReportResponseDto(newReport);
   }
 
-  editReport(body: UpdateReportData, id: string, type: ReportType) {
+  editReport(
+    body: UpdateReportData,
+    id: string,
+    type: ReportType,
+  ): ReportResponseDto {
     const reportToUpdate = data.report
       .filter((item) => item.type === type)
       .find((item) => item.id === id);
 
     if (!reportToUpdate) {
-      return 'Report Not found';
+      return;
+      // return 'Report Not found';
     }
 
     const reportToUpdateIndex = data.report.findIndex((item) => item.id === id);
@@ -71,7 +72,7 @@ export class AppService {
 
     data.report[reportToUpdateIndex] = reportUpdated;
 
-    return reportUpdated;
+    return new ReportResponseDto(reportUpdated);
   }
 
   deleteReport(id: string) {
