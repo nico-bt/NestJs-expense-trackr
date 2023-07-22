d61013e615833d6cacdadce987948cecb3ba946d
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-07-22 00:02:32 -0300

    Add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,17 @@ import { ValidationPipe } from '@nestjs/common/pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true })); //Whitelist elimina del body lo que no esté definido en el dto
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //Whitelist elimina del body lo que no esté definido en el dto
+      whitelist: true,
+      // tramnsforma con en reponseDto constructor armado en Dto
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
